e0a24116db0986a1e460605613bc5d05772a7e41
Zeeshan Khan <109348123+zeeshankhanbrainium@users.noreply.github.com>
2023-06-28 12:59:54 +0530

    Initial Template

--- lib/app/data/utils/color_manager.dart
+++ lib/app/data/utils/color_manager.dart
@@ -5,4 +5,6 @@ class ColorManager {
 
   static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF000000);
+  static const primary = Color(0xFF81B2C6);
+  static const button = Color(0xFF508CAE);
 }
