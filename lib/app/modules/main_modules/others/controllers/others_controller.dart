a1028aad6ab95cdc80fa711896e7e0688a238433
Jahir <jahir.abbas@brainiuminfotech.com>
2023-07-04 12:36:44 +0530

    Others page added

--- lib/app/modules/main_modules/others/controllers/others_controller.dart
+++ lib/app/modules/main_modules/others/controllers/others_controller.dart
@@ -1,9 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class OthersController extends GetxController {
   //TODO: Implement OthersController
 
   final count = 0.obs;
+  var isEnable = true.obs;
+  RxString dropdownValue = "1".obs;
+  var total = 0.0.obs;
+  final othersKey = GlobalKey<FormState>();
+  final labelController1 = TextEditingController();
+  final labelController2 = TextEditingController();
+  final labelController3 = TextEditingController();
+  final labelController4 = TextEditingController();
+  final labelController5 = TextEditingController();
+  final labelController6 = TextEditingController();
+  final labelController7 = TextEditingController();
+  final labelController8 = TextEditingController();
+  final labelController9 = TextEditingController();
+  final firstNameNode = FocusNode();
+  final labelControllerNode1 = FocusNode();
+  final labelControllerNode2 = FocusNode();
+  final labelControllerNode3 = FocusNode();
+  final labelControllerNode4 = FocusNode();
+  final labelControllerNode5 = FocusNode();
+  final labelControllerNode6 = FocusNode();
+  final labelControllerNode7 = FocusNode();
+  final labelControllerNode8 = FocusNode();
+  final labelControllerNode9 = FocusNode();
+
+
   @override
   void onInit() {
     super.onInit();
@@ -20,4 +46,17 @@ class OthersController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  void updateButtonState(dynamic value) {
+    if (!othersKey.currentState!.validate() ||
+        labelController1.text.isEmpty ||
+        labelController2.text.isEmpty ||
+        labelController3.text.isEmpty ||
+        labelController4.text.isEmpty ||
+        labelController5.text.isEmpty) {
+      isEnable.value = false;
+    } else {
+      isEnable.value = true;
+    }
+  }
 }
--- lib/app/modules/main_modules/others/views/others_view.dart
+++ lib/app/modules/main_modules/others/views/others_view.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_outdoor_footprint/app/modules/main_modules/others/widgets/others_widget.dart';
 
 import '../../../../data/utils/color_manager.dart';
 import '../../../../data/utils/string_manager.dart';
 import '../../../../data/utils/widget_manager.dart';
 
+import '../../gear/widgets/gearwidget.dart';
 import '../controllers/others_controller.dart';
 
 class OthersView extends GetView<OthersController> {
@@ -25,8 +28,280 @@ class OthersView extends GetView<OthersController> {
             padding: EdgeInsets.symmetric(
               horizontal: 20.w,
             ),
-            child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  StringManager.logging,
+                  style: GoogleFonts.oswald(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18.sp,
+                    color: ColorManager.titleText,
+                  ),
+                ),
+                SizedBox(height: 16.h,),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10.h,
+                  ),
+                  margin: EdgeInsets.only(
+                    top: 8.h,
+                    //bottom: 20.h,
+                  ),
+                  decoration: BoxDecoration(
+                      color: ColorManager.white,
+                      borderRadius: BorderRadius.circular(
+                        8.w,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: ColorManager.button.withOpacity(
+                            0.2,
+                          ),
+                          spreadRadius: 1,
+                          blurRadius: 10,
+                        )
+                      ]),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: OthersWidgets.editFields(
+                      controller: controller.labelController1,
+                      node: controller.firstNameNode,
+                      type: FieldType.firstName,
+                      onChanged: (value) async {
+                        controller.updateButtonState(value);
+                      },
+                      labelName: StringManager.othersLabel1,
+                    ),
+                  )
 
+                ),
+                SizedBox(height: 8.h,),
+                Text(
+                  StringManager.skiDay,
+                  style: GoogleFonts.oswald(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18.sp,
+                    color: ColorManager.titleText,
+                  ),
+                ),
+                SizedBox(height: 8.h,),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.h,
+                    ),
+                    margin: EdgeInsets.only(
+                      top: 8.h,
+                      //bottom: 20.h,
+                    ),
+                    decoration: BoxDecoration(
+                        color: ColorManager.white,
+                        borderRadius: BorderRadius.circular(
+                          8.w,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorManager.button.withOpacity(
+                              0.2,
+                            ),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                          )
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SizedBox(),
+                    )
+
+                ),
+
+                //Food Section
+                SizedBox(height: 8.h,),
+                Text(
+                  StringManager.food,
+                  style: GoogleFonts.oswald(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18.sp,
+                    color: ColorManager.titleText,
+                  ),
+                ),
+                SizedBox(height: 8.h,),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.h,
+                    ),
+                    margin: EdgeInsets.only(
+                      top: 8.h,
+                      //bottom: 20.h,
+                    ),
+                    decoration: BoxDecoration(
+                        color: ColorManager.white,
+                        borderRadius: BorderRadius.circular(
+                          8.w,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorManager.button.withOpacity(
+                              0.2,
+                            ),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                          )
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SizedBox(),
+                    )
+
+                ),
+
+                //Others Section
+                SizedBox(height: 8.h,),
+                Text(
+                  StringManager.others,
+                  style: GoogleFonts.oswald(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18.sp,
+                    color: ColorManager.titleText,
+                  ),
+                ),
+                SizedBox(height: 8.h,),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.h,
+                    ),
+                    margin: EdgeInsets.only(
+                      top: 8.h,
+                      //bottom: 20.h,
+                    ),
+                    decoration: BoxDecoration(
+                        color: ColorManager.white,
+                        borderRadius: BorderRadius.circular(
+                          8.w,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorManager.button.withOpacity(
+                              0.2,
+                            ),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                          )
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: SizedBox(),
+                    )
+
+                ),
+
+                //New Car section
+                SizedBox(height: 8.h,),
+                Text(
+                  StringManager.newCar,
+                  style: GoogleFonts.oswald(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18.sp,
+                    color: ColorManager.titleText,
+                  ),
+                ),
+                SizedBox(height: 8.h,),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.h,
+                    ),
+                    margin: EdgeInsets.only(
+                      top: 8.h,
+                      //bottom: 20.h,
+                    ),
+                    decoration: BoxDecoration(
+                        color: ColorManager.white,
+                        borderRadius: BorderRadius.circular(
+                          8.w,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: ColorManager.button.withOpacity(
+                              0.2,
+                            ),
+                            spreadRadius: 1,
+                            blurRadius: 10,
+                          )
+                        ]),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded (
+                                flex:1,
+                                child : GearWidget.textWithField(
+                                  fieldName: StringManager.othersLabel17,
+                                  isEnable: controller.isEnable,
+                                  dropdownvalue: "1",
+                                ),
+                              ),
+                              SizedBox(width: 8.w,),
+                              Expanded (
+                                flex:1,
+                                child : GearWidget.textWithField(
+                                  fieldName: StringManager.othersLabel18,
+                                  isEnable: controller.isEnable,
+                                  dropdownvalue: "1",
+                                ),
+                              ),
+                            ],
+                          ),
+                          GearWidget.textWithField(
+                            fieldName: StringManager.othersLabel19,
+                            isEnable: controller.isEnable,
+                            dropdownvalue: "1",
+                          ),
+                        ],
+                      ),
+                    )
+
+                ),
+
+                WidgetManager.primaryButton(
+                  buttonName: StringManager.submit,
+                  isEnable: controller.isEnable,
+                  onTap: () async {},
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${StringManager.total}: ',
+                      style: GoogleFonts.oswald(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20.sp,
+                        color: ColorManager.button,
+                      ),
+                    ),
+                    Obx(() {
+                      final totalValue = controller.total.value;
+                      final displayValue = totalValue.isNaN ? 0.0 : totalValue;
+                      return Text(
+                        '$displayValue ${StringManager.kgProduced}',
+                        style: GoogleFonts.oswald(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20.sp,
+                          color: ColorManager.displayText,
+                        ),
+                      );
+                    }),
+                  ],
+                ),
+              ],
             ),
           ),
         )
