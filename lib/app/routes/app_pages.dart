490743fe7db037492d95574fefc292af30016031
Zeeshan Khan <109348123+zeeshankhanbrainium@users.noreply.github.com>
2023-07-04 13:29:55 +0530

    Calculator - Mobility

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -34,7 +34,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.MOBILITY;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
