bf57dc0a6c7c5f2f4df3c638bd5fa482ef1e9851
Zeeshan Khan <109348123+zeeshankhanbrainium@users.noreply.github.com>
2023-07-21 18:35:25 +0530

    Notification

--- lib/app/modules/misc_modules/notification/widgets/notification_widget.dart
+++ lib/app/modules/misc_modules/notification/widgets/notification_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_outdoor_footprint/app/data/utils/asset_manager.dart';
 
@@ -12,78 +13,80 @@ class NotificationWidget {
   static Widget customNotification({
     required dynamic list,
   }) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const BouncingScrollPhysics(),
-      itemCount: list.length,
-      padding: EdgeInsets.only(
-        bottom: 15.h,
-      ),
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 15.w,
-          ),
-          child: Container(
+    return Obx(() {
+      return ListView.builder(
+        shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
+        itemCount: list.length,
+        padding: EdgeInsets.only(
+          bottom: 15.h,
+        ),
+        itemBuilder: (context, index) {
+          return Padding(
             padding: EdgeInsets.symmetric(
               horizontal: 15.w,
-              vertical: 10.h,
             ),
-            margin: EdgeInsets.only(
-              top: 10.h,
-            ),
-            decoration: BoxDecoration(
-              color: ColorManager.white,
-              borderRadius: BorderRadius.circular(
-                10.w,
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15.w,
+                vertical: 10.h,
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: ColorManager.boxShadow.withOpacity(
-                    0.3,
-                  ),
-                  spreadRadius: 1,
-                  blurRadius: 10,
+              margin: EdgeInsets.only(
+                top: 10.h,
+              ),
+              decoration: BoxDecoration(
+                color: ColorManager.white,
+                borderRadius: BorderRadius.circular(
+                  10.w,
                 ),
-              ],
-            ),
-            child: Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 10.w,
-                  ),
-                  child: SvgPicture.asset(
-                    AssetManager.notificationSnackBar,
-                    width: 20.w,
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      list[index]['title'],
-                      style: GoogleFonts.oswald(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18.sp,
-                        color: ColorManager.displayText,
-                      ),
+                boxShadow: [
+                  BoxShadow(
+                    color: ColorManager.boxShadow.withOpacity(
+                      0.3,
                     ),
-                    Text(
-                      list[index]['content'],
-                      style: GoogleFonts.oswald(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14.sp,
-                        color: ColorManager.labelText,
-                      ),
+                    spreadRadius: 1,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: 10.w,
                     ),
-                  ],
-                )
-              ],
+                    child: SvgPicture.asset(
+                      AssetManager.notificationSnackBar,
+                      width: 20.w,
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        list[index]['title'],
+                        style: GoogleFonts.oswald(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18.sp,
+                          color: ColorManager.displayText,
+                        ),
+                      ),
+                      Text(
+                        list[index]['content'],
+                        style: GoogleFonts.oswald(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14.sp,
+                          color: ColorManager.labelText,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        );
-      },
-    );
+          );
+        },
+      );
+    });
   }
 }
